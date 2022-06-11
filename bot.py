0668604de3bf17e324c666577d029b9c926f2f75
alex-bellon <alexrbellon@gmail.com>
2022-06-11 14:36:47 -0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+# bot.py
+import discord, os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+archive_id = 985292956112158773
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author != client.user: 
+        await message.add_reaction('✅')
+
+@client.event
+async def on_reaction_add(reaction, user):
+    if reaction.emoji == '✅' and user != client.user and reaction.message.channel != archive_id:
+        await reaction.message.delete()
+        await client.get_channel(archive_id).send(reaction.message.content)
+
+client.run(TOKEN)
+
